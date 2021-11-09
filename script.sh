0437f8d6180670cd5f9b0d3e2a7830bf6fa95d0b
pimanDE <pimande@gmx.de>
2021-11-09 21:43:15 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Weboberfläche des Pi-hole auf deutsch übersetzen
+# getestet auf Pi-hole Version v5.5 - FTL Version v5.11 - Web Interface Version v5.8
+#
+# Benutzung auf eigene Gefahr!!!
+#
+####################################################################################################################
+# Setzen der Variablen
+
+username=`whoami`
+hostname=`hostname`
+ipadresse=`hostname -I`
+date=`date +'%Y%m%d-%H%M%S'`
+
+standard="\033[0m"
+grau="\033[1;30m"
+rotfett="\033[1;31m"
+blaufett="\033[1;34m"
+gruenfett="\033[1;32m"
+
+info="[i]"
+over="\\r\\033[K"
+fehler="[${rotfett}✗${standard}]"
+haken="[${gruenfett}✓${standard}]"
+done="${gruenfett} done!${standard}"
+
+touch /tmp/error-translate.log
+exec 2> /tmp/error-translate.log
+
+####################################################################################################################
+# Start des Script's
+
+clear
+
+if dpkg-query -s rpl 2>/dev/null|grep -q installed; then
+    echo "rpl schon ist installiert ..." >> /dev/null
+else
+    echo
+    echo -e "${blaufett}   rpl muss installiert werden ...${standard}"
+    echo
+    echo
+    sudo apt install -y rpl
+fi
+
+
+echo
+echo -e "${blaufett}   Übersetze die Pi-hole Weboberfläche auf deutsch ...${standard}"
+echo
+echo -e "${blaufett}   Dies kann einige Minuten dauern ...${standard}"
+echo
+
+
+
+
+sudo rpl 'aa' 'bb' ~/Scripte/datei.txt
+
+exit
